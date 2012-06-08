69a5444c069f14e18e308169e0b1cb0938c83926
craig t mackenzie <craigtmackenzie@gmail.com>
2012-06-08 11:58:12 +0200

    we don't want the homepage overwritten, just give us the page object.

--- app/controllers/spree/pages_controller.rb
+++ app/controllers/spree/pages_controller.rb
@@ -3,11 +3,11 @@ class Spree::PagesController < Spree::BaseController
   def show
     @page = current_page
     raise ActionController::RoutingError.new("No route matches [GET] #{request.path}") if @page.nil? 
-    if @page.root?
-      @posts    = Spree::Post.live.limit(5)    if SpreeEssentials.has?(:blog)
-      @articles = Spree::Article.live.limit(5) if SpreeEssentials.has?(:news)
-      render :template => 'spree/pages/home'
-    end
+#    if @page.root?
+ #     @posts    = Spree::Post.live.limit(5)    if SpreeEssentials.has?(:blog)
+  #    @articles = Spree::Article.live.limit(5) if SpreeEssentials.has?(:news)
+#      render :template => 'spree/pages/home'
+ #   end
   end
 
   private
