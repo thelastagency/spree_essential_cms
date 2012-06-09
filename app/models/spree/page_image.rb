61226df0838352297ebd3477bbf002afd44a000d
craig t mackenzie <craigtmackenzie@gmail.com>
2012-06-09 21:04:47 +0100

    support the image sizes we need

--- app/models/spree/page_image.rb
+++ app/models/spree/page_image.rb
@@ -5,7 +5,8 @@ class Spree::PageImage < Spree::Asset
   validates_attachment_presence :attachment
   
   has_attached_file :attachment,
-    :styles => Proc.new{ |clip| clip.instance.attachment_sizes },
+    # :styles => Proc.new{ |clip| clip.instance.attachment_sizes },
+    :styles => { :mini => '48x48>', :medium => '427x287>', :hero => '720x430#', :hub_thumb => "95x65#", :hub_info => "245x180#" },
     :default_style => :medium,
     :url => '/spree/pages/:id/:style/:basename.:extension',
     :path => ':rails_root/public/spree/pages/:id/:style/:basename.:extension'
@@ -15,7 +16,7 @@ class Spree::PageImage < Spree::Asset
   end
 
   def attachment_sizes
-    sizes = {}
+    sizes = { :mini => '48x48>', :medium => '427x287>', :hero => '720x430#', :hub_thumb => "95x65#", :hub_info => "245x180#" }
     if image_content?
       sizes.merge!(:mini => '48x48>', :small => '150x150>', :medium => '420x300>', :large => '900x650>')
       sizes.merge!(:slide => '950x250#') if viewable.respond_to?(:root?) && viewable.root?
@@ -24,3 +25,8 @@ class Spree::PageImage < Spree::Asset
   end
 
 end
+
+
+# Image.attachment_definitions[:attachment][:styles].merge!(
+#   :mini => '48x48>', :medium => '427x287>', :hero => '720x430#', :hub_thumb => "95x65#", :hub_info => "245x180#"
+# )
