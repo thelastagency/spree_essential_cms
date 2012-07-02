068c392020a176613a1c30f180e05ebfbeb71358
craig t mackenzie <craigtmackenzie@gmail.com>
2012-07-02 14:00:58 +0100

    hack admin to show all pages so reordering works

--- app/controllers/spree/pages_controller.rb
+++ app/controllers/spree/pages_controller.rb
@@ -2,18 +2,18 @@ class Spree::PagesController < Spree::BaseController
 
   def show
     @page = current_page
-    raise ActionController::RoutingError.new("No route matches [GET] #{request.path}") if @page.nil? 
-#    if @page.root?
- #     @posts    = Spree::Post.live.limit(5)    if SpreeEssentials.has?(:blog)
-  #    @articles = Spree::Article.live.limit(5) if SpreeEssentials.has?(:news)
-#      render :template => 'spree/pages/home'
- #   end
+    raise ActionController::RoutingError.new("No route matches [GET] #{request.path}") if @page.nil?
+    #    if @page.root?
+    #     @posts    = Spree::Post.live.limit(5)    if SpreeEssentials.has?(:blog)
+    #    @articles = Spree::Article.live.limit(5) if SpreeEssentials.has?(:news)
+    #      render :template => 'spree/pages/home'
+    #   end
   end
 
   private
 
-    def accurate_title
-      @page.meta_title
-    end
+  def accurate_title
+    @page.meta_title
+  end
 
 end
